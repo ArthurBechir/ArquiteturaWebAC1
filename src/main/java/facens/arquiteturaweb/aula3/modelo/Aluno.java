b077c95dc9109e9b85e41f4bf5a3cae4b6de02a1
ArthurBechir <costela.bechir@gmail.com>
2024-04-03 14:26:57 -0300

    aula 5

---
+++ src/main/java/facens/arquiteturaweb/aula3/modelo/Aluno.java
@@ -0,0 +1,68 @@
+package facens.arquiteturaweb.aula3.modelo;
+
+public class Aluno {
+    private Long id;
+    private String nome;
+    private String curso;
+    private String ra;
+    private String tipoensino;
+    private String genero;
+
+    public Aluno(Long id, String nome, String curso, String ra, String tipoensino, String genero) {
+        this.id = id;
+        this.nome = nome;
+        this.curso = curso;
+        this.ra = ra;
+        this.tipoensino = tipoensino;
+        this.genero = genero;
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getRa() {
+        return ra;
+    }
+
+    public void setRa(String ra) {
+        this.ra = ra;
+    }
+
+    public String getTipoEnsino() {
+        return tipoensino;
+    }
+
+    public void setTipoEnsino(String tipoensino) {
+        this.tipoensino = tipoensino;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+}
