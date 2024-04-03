85adf59bc59b6424e0c25ea632284f6f852e6c96
ArthurBechir <costela.bechir@gmail.com>
2024-04-03 12:06:55 -0300

    aulas 3 e 4

---
+++ src/main/java/facens/arquiteturaweb/ac1/service/AlunoService.java
@@ -0,0 +1,13 @@
+package facens.arquiteturaweb.ac1.service;
+
+import facens.arquiteturaweb.ac1.modelo.Aluno;
+
+import java.util.List;
+
+public interface AlunoService {
+    List<Aluno> getAllAlunos();
+
+    Aluno getAlunoById(Long id);
+
+    Aluno createAluno(Aluno aluno);
+}
